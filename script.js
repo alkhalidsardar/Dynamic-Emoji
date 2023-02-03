97680f27e135c7008db6622130ee0fccbcc3d25a
Al Khalid <114647047+alkhalidsardar@users.noreply.github.com>
2023-02-04 00:21:52 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener("DOMContentLoaded",() => {
+	const fr = new FaceRating("#face-rating");
+});
+
+class FaceRating {
+	constructor(qs) {
+		this.input = document.querySelector(qs);
+		this.input?.addEventListener("input",this.update.bind(this));
+		this.face = this.input?.previousElementSibling;
+		this.update();
+	}
+	update(e) {
+		let value = this.input.defaultValue;
+
+		// when manually set
+		if (e) value = e.target?.value;
+		// when initiated
+		else this.input.value = value;
+
+		const min = this.input.min || 0;
+		const max = this.input.max || 100;
+		const percentRaw = (value - min) / (max - min) * 100;
+		const percent = +percentRaw.toFixed(2);
