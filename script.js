78aea0a5008a1b4ba33bd980e3c2b134a70a4a4d
Al Khalid <114647047+alkhalidsardar@users.noreply.github.com>
2023-02-04 00:35:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,45 @@ class FaceRating {
 		const max = this.input.max || 100;
 		const percentRaw = (value - min) / (max - min) * 100;
 		const percent = +percentRaw.toFixed(2);
+
+		this.input?.style.setProperty("--percent",`${percent}%`);
+
+		// face and range fill colors
+		const maxHue = 120;
+		const hueExtend = 30;
+		const hue = Math.round(maxHue * percent / 100);
+
+		let hue2 = hue - hueExtend;
+		if (hue2 < 0) hue2 += 360;
+
+		const hues = [hue,hue2];
+		hues.forEach((h,i) => {
+			this.face?.style.setProperty(`--face-hue${i + 1}`,h);
+		});
+
+		this.input?.style.setProperty("--input-hue",hue);
+
+		// emotions
+		const duration = 1;
+		const delay = -(duration * 0.99) * percent / 100;
+		const parts = ["right","left","mouth-lower","mouth-upper"];
+
+		parts.forEach(p => {
+			const el = this.face?.querySelector(`[data-${p}]`);
+			el?.style.setProperty(`--delay-${p}`,`${delay}s`);
+		});
+
+		// aria label
+		const faces = [
+			"Sad face",
+			"Slightly sad face",
+			"Straight face",
+			"Slightly happy face",
+			"Happy face"
+		];
+		let faceIndex = Math.floor(faces.length * percent / 100);
+		if (faceIndex === faces.length) --faceIndex;
+
+		this.face?.setAttribute("aria-label",faces[faceIndex]);
+	}
+}
